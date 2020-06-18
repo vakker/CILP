4dfe277a31e57e86280f850c776bbbd2c30b582a
Vince Jankovics <vincejankovics@gmail.com>
2020-06-18 17:44:19 +0100

    BCP cleanup

--- cilp/bcp.py
+++ cilp/bcp.py
@@ -1,17 +1,16 @@
 import json
-import logging
 import re
 import tempfile
+import time
 from os import path as osp
 
-from .utils import (aleph_settings, create_script, execute, get_aleph,
-                    load_examples, run_aleph)
+from .utils import aleph_settings, create_script, load_examples, run_aleph
 
 
 def run_bcp(data_dir, cached=True, print_output=False):
     bc_file = osp.join(data_dir, 'bc.json')
     if osp.exists(bc_file) and cached:
-        logging.info('Loading from cache')
+        print('Loading from cache')
         return
 
     bottom_clauses = {}
@@ -23,9 +22,7 @@ def run_bcp(data_dir, cached=True, print_output=False):
         bk_file = osp.join(data_dir, 'bk.pl')
         mode_file = osp.join(data_dir, 'mode.pl')
 
-        script_lines = aleph_settings(mode_file,
-                                      bk_file,
-                                      data_files={'train_pos': train_pos})
+        script_lines = aleph_settings(mode_file, bk_file, data_files={'train_pos': train_pos})
         # script_lines += [f':- set(train_pos, "{train_pos}").']
         for i in range(len(pos_examples)):
             script_lines += [f':- sat({i+1}).']
@@ -33,15 +30,17 @@ def run_bcp(data_dir, cached=True, print_output=False):
         temp_dir = tempfile.mkdtemp()
         script_file = create_script(temp_dir, script_lines)
 
-        logging.info('Running Prolog script...')
+        print(f'Running Prolog script {script_file}')
+        start_time = time.time()
         prolog_output = run_aleph(script_file)
-        logging.info('Prolog done, parsing output...')
+        time_elapsed = time.time() - start_time
+        print(f'Prolog done, took {time_elapsed:.1f} parsing output...')
 
         if print_output:
-            logging.debug(prolog_output)
+            print(prolog_output)
 
-        bottom_clauses_raw = re.findall(
-            r'\[bottom clause\]\n(.*?)\n\[literals\]', prolog_output, re.S)
+        bottom_clauses_raw = re.findall(r'\[bottom clause\]\n(.*?)\n\[literals\]', prolog_output,
+                                        re.S)
 
         for b in bottom_clauses_raw:
             clause = re.sub(r'[ \n]', '', b).split(':-')
